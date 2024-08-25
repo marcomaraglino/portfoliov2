3f433f3fcb95ee152a1cee4845609a4bb2ed9f2d
marco <marcomaraglino5@gmail.com>
2024-08-25 19:21:50 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify('1.0.0'),
+  },
   base: '/portfoliowebsite/'
 })
