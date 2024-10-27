b8e1542f39d7c51f57a5cf0512277c33ebf216fb
marco <marcomaraglino5@gmail.com>
2024-10-27 12:47:05 +0100

    homepage fixed padding

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
         >
-        <div className="container mx-auto py-16">
+        <div className="container mx-auto py-56">
             <div className="max-sm:w-full grid grid-cols-1 md:grid-cols-1 gap-8 w-1/2">
             <p className="text-left">I'm a dedicated programmer driven by a relentless curiosity for learning, a passion for problem-solving,
                 and an insatiable thirst for new experiences. With a deep love for coding and a penchant for
