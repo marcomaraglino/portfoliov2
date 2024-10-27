86647aa691ba2b599426c08658f237b323ab0b5c
marco <marcomaraglino5@gmail.com>
2024-10-27 12:50:22 +0100

    YT Social added

--- src/pages/social.jsx
+++ src/pages/social.jsx
@@ -10,10 +10,11 @@ const Social = () => {
         <div className="container py-16">
             <div className="grid grid-cols-1 md:grid-cols-1 gap-8">
                 <ul className="text-left">
-                    <li><a className="hover:duration-100 duration-100 hover:text-fuchsia-600 font-normal" href="https://www.instagram.com/marcomaraglino/">Instagram</a></li>
-                    <li><a className="hover:duration-100 duration-100 hover:text-black font-normal" href="https://twitter.com/AuMarcom">X</a></li>
-                    <li><a className="hover:duration-100 duration-100 hover:text-blue-600 font-normal" href="https://www.linkedin.com/in/marcomaraglino5/">Linkedin</a></li>
-                    <li><a className="hover:duration-100 duration-100 hover:text-gray-600 font-normal" href="https://github.com/marcomaraglino">GitHub</a></li>
+                    <li><a className="hover:duration-100 duration-100 hover:text-fuchsia-600 font-normal" target="_blank" href="https://www.instagram.com/marcomaraglino/">Instagram</a></li>
+                    <li><a className="hover:duration-100 duration-100 hover:text-black font-normal" target="_blank" href="https://twitter.com/AuMarcom">X</a></li>
+                    <li><a className="hover:duration-100 duration-100 hover:text-blue-600 font-normal" target="_blank" href="https://www.linkedin.com/in/marcomaraglino5/">Linkedin</a></li>
+                    <li><a className="hover:duration-100 duration-100 hover:text-gray-600 font-normal" target="_blank" href="https://github.com/marcomaraglino">GitHub</a></li>
+                    <li><a className="hover:duration-100 duration-100 hover:text-red-600 font-normal" target="_blank" href="https://www.youtube.com/@marco.maraglino">YouTube</a></li>
                 </ul>
             </div>
         </div>
